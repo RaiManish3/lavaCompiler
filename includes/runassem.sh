91c3fdb183dfa89b20c7c03aa909e4d5c9904594
RaiManish3 <manish.07.1996@gmail.com>
2018-02-11 19:16:55 +0530

    script update to show errors

--- includes/runassem.sh
+++ includes/runassem.sh
@@ -1,7 +1,8 @@
 filename=$1
-make
-bin/codegen "test/$filename.ir" >/dev/null 2>&1
-nasm -f elf32 "bnf/$filename.s"
-gcc -m32 "bnf/$filename.o" -o "bnf/a.out"
-bnf/a.out
+make &&
+bin/codegen "test/$filename.ir" 2>&1 >/dev/null &&
+mkdir -p "bnf/" &&
+nasm -f elf32 "bnf/$filename.s" &&
+gcc -m32 "bnf/$filename.o" -o "bnf/a.out" &&
+bnf/a.out &&
 rm "bnf/a.out" "bnf/$filename.o"
