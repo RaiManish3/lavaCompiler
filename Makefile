c8780b9c1b814993bd414c30cee5ae09abfabb9a
RaiManish3 <manish.07.1996@gmail.com>
2018-02-26 06:58:47 +0530

    simple expression done

--- Makefile
+++ Makefile
@@ -3,7 +3,11 @@ SRC=src
 BIN=bin
 TEST=test
 
-.DEFAULT_GOAL := ass2
+.DEFAULT_GOAL := ass3
+
+ass3: $(SRC)/myParser.py $(SRC)/myLexer.py $(SRC)/htmlWriter.py $(SRC)/parserHtmlScipt.sh
+	@cp $(SRC)/parserHtmlScipt.sh $(BIN)/parser
+	@chmod +x $(BIN)/parser
 
 ass2: $(SRC)/myCodeGen.py
 	@mkdir -p $(BIN)
@@ -16,4 +20,4 @@ ass1: $(SRC)/assignment1.py $(SRC)/myLexer.py
 	@chmod +x $(BIN)/lexer
 
 clean:
-	@rm -rf bin/* src/{*.pyc,__pycache__,lextab.py} includes/{*.pyc,__pycache__}
+	@rm -rf bin/* src/{*.pyc,__pycache__,lextab.py,parsetab.py,parser.out} includes/{*.pyc,__pycache__}
--- src/htmlWriter.py
+++ src/htmlWriter.py
@@ -5,16 +5,20 @@ path.extend(['.','..'])
 
 import os.path
 import re
+from myLexer import MyLexer
 
 ## GLOBALS
 
+tokens = MyLexer.tokens
 FLAG_LITERAL = 0
 FLAG_BOP = 1
 FLAG_UOP = 2
 
+
+## TODO :: EXPAND THE LIST TO COVER UP THE TOKEN SET
 literal_tokens = ['INTEGER_LITERAL']
 bop_tokens = ['PLUS', 'MINUS', 'MULTIPLY', 'DIVIDE']
-uop_tokens = ['PLUS','MINUS'] ## TODO :: resolve conflict here
+uop_tokens = ['PLUS','MINUS']
 
 def extractHelper(stx, regMatch, flag):
     if flag == FLAG_LITERAL: 
@@ -57,13 +61,56 @@ def getReduceRules(fd):
     return reducedString
 
 
-def reverseLines(reducedString):
-    reversedString = '\n'.join(reversed(reducedString.split('\n')))
-    return reversedString
+def stepReduction(reducedString):
+    reversedList = reversed(reducedString.split('\n')[:-1])
+    stepCode = "program\n"
+    prevLine = stepCode
+
+    for i in reversedList:
+        lhs, rhs = i.split(' -> ')
+        length_lhs = len(lhs)
+        ruleIndex = prevLine.rfind(lhs)
+        thisLine = prevLine[:ruleIndex] + rhs + prevLine[ruleIndex+length_lhs:]
+        stepCode += thisLine
+        prevLine = thisLine
+
+    return stepCode
 
 
-def beautifyHtml(reversedString):
-    pass
+# TODO :: HANDLE ERRORS
+def beautifyHtml(stepCode):
+    htmlCode = """
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <title> cs335 asgn3 </title>
+        <style>
+            p{
+                background-color: rgba(240,240,240,0.4);
+                justify-content: center;
+                margin-bottom: 5px;
+            }
+        </style>
+        </head>
+        <body>
+            <div>
+   """
+
+    stepList = stepCode.split('\n')
+    for line in stepList[:-1]:
+        htmlCode += "\t\t<p>"
+        words = line.split()
+        for word in words:
+            if word in tokens:
+                htmlCode += "<b>" + word + "</b> "
+            else:
+                htmlCode += word + " "
+
+        htmlCode += "</p>\n"
+
+    htmlCode += "\t    </div>\n\t</body>\n\t</html>"
+    return htmlCode
+  
 
 if __name__ == "__main__":
     ## for now assume that parser debug is written into a file
@@ -71,8 +118,17 @@ if __name__ == "__main__":
     if os.path.exists(filename):
         fd = open(filename, 'r')
         reducedString = getReduceRules(fd)
-        reversedString = reverseLines(reducedString)
-        htmlCode = beautifyHtml(reversedString)
+        stepCode = stepReduction(reducedString)
+        htmlCode = beautifyHtml(stepCode)
         fd.close()
+        try:
+            directory = "bin/"
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            with open(directory + "htmlCode.html", 'w') as fs:
+                fs.write(htmlCode)
+        except:
+            print("Cannot write to file!")
+            exit(EXIT_FAILURE)
     else:
         print("File Does Not Exist ...")
